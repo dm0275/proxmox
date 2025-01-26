b555649f38d7ab5e941052ae9869c2766ecf1681
Daniel Mancilla <dm0275@gmail.com>
2025-01-25 22:02:24 -0600

    Updated tf config

--- terraform/proxmox.tf
+++ terraform/proxmox.tf
@@ -5,21 +5,23 @@ resource "proxmox_vm_qemu" "instance" {
     target_node = var.proxmox_node           # Proxmox node to deploy the instance on
     vmid        = var.instance_vm_id         # Unique VM ID for the instance
     clone       = var.template_name          # Name of the Proxmox template to clone
-    ciuser = var.vm_username
-    cipassword = var.vm_password
-    sshkeys = var.public_ssh_key
+    ciuser      = var.vm_username            # Cloud-init user account for the VM
+    cipassword  = var.vm_password            # Cloud-init user password for the VM
+    sshkeys     = var.public_ssh_key         # SSH public key for secure access to the VM
 
-    # Disk configuration
+    # Disk configuration for cloud-init
     disk {
-        slot = "ide0"
-        type    = "cloudinit"
-        storage = "local-lvm"
+        slot    = "ide0"                     # Disk slot for cloud-init ISO configuration
+        type    = "cloudinit"                # Disk type for cloud-init data
+        storage = "local-lvm"                # Proxmox storage pool for cloud-init disk
     }
+
+    # Main disk configuration
     disk {
         slot    = "virtio0"                  # Specify the disk slot (virtio for better performance)
         size    = var.disk_size              # Size of the disk for the instance (e.g., "50G")
-        type    = "disk"                     # Disk type
-        storage = "local-lvm"                # Proxmox storage pool for the disk
+        type    = "disk"                     # Disk type for VM storage
+        storage = "local-lvm"                # Proxmox storage pool for the main disk
     }
 
     # CPU configuration
@@ -38,7 +40,7 @@ resource "proxmox_vm_qemu" "instance" {
     onboot = true                            # Ensure the instance starts automatically when Proxmox boots
 
     # Network configuration
-    ipconfig0 = "ip=dhcp"
+    ipconfig0 = "ip=dhcp"                    # Configure network interface to use DHCP for IP assignment
     network {
         id     = 0                           # Network interface ID
         bridge = "vmbr0"                     # Network bridge to connect the instance to
@@ -46,10 +48,14 @@ resource "proxmox_vm_qemu" "instance" {
     }
 }
 
+# Resource to configure the Proxmox VM after creation
 resource "terraform_data" "configure-vm" {
+    # Trigger reconfiguration when the VM ID changes
     triggers_replace = [
         proxmox_vm_qemu.instance.id
     ]
+
+    # SSH connection settings
     connection {
         type     = "ssh"
         user     = var.vm_username
@@ -57,6 +63,7 @@ resource "terraform_data" "configure-vm" {
         host     = proxmox_vm_qemu.instance.ssh_host
     }
 
+    # Provisioner to execute remote commands
     provisioner "remote-exec" {
         inline = [
             "sudo apt update"
@@ -70,7 +77,7 @@ output "instance_details" {
     value = {
         name        = proxmox_vm_qemu.instance.name        # Name of the Proxmox instance
         vmid        = proxmox_vm_qemu.instance.vmid        # VM ID of the created instance
-        vmip        = proxmox_vm_qemu.instance.ssh_host   # IP config for the VM
+        vmip        = proxmox_vm_qemu.instance.ssh_host    # IP address of the VM for SSH access
         target_node = proxmox_vm_qemu.instance.target_node # Proxmox node where the instance is deployed
     }
 }
